71a09f583ac4fe700f411abb8ddc5f91ed9c1bff
trettifjerde <sasjandreeva@gmail.com>
2024-02-26 23:15:09 +0100

    paddings upd

--- src/components/main/project.tsx
+++ src/components/main/project.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
 import { ProjectInfo } from '../../utils/types';
-import {leaves} from '../../styles/leaves.module.css';
-import {info, accountable, links} from './project.module.css';
+import { leaves } from '../../styles/leaves.module.css';
+import { info, accountable, links } from './project.module.css';
 
 const Project = ({ project }: { project: ProjectInfo }) => <article>
   <h1>{project.frontmatter?.name}</h1>
 
   <div className={info}>
-    <div className={accountable}>
-      {project.frontmatter?.accountable?.map(item => <p key={item}>{item}</p>)}
-    </div>
-
     <h5>{project.frontmatter?.desc}</h5>
 
-    <div className={`${leaves} ${links}`}>
+    <div>
 
-      {project.frontmatter?.site && <a href={project.frontmatter?.site || '#'}>
-        <h3>site</h3>
-      </a>}
+      <div className={accountable}>
+        {project.frontmatter?.accountable?.map(item => <p key={item}>{item}</p>)}
+      </div>
 
-      {project.frontmatter?.repo && <a href={project.frontmatter.repo}>
-        <h3>repo</h3>
-      </a>
-      }
+      <div className={`${leaves} ${links}`}>
+        {project.frontmatter?.site && <a href={project.frontmatter?.site || '#'}>
+          <h3>site</h3>
+        </a>}
+
+        {project.frontmatter?.repo && <a href={project.frontmatter.repo}>
+          <h3>repo</h3>
+        </a>
+        }
+      </div>
+
     </div>
-
   </div>
 
   <section dangerouslySetInnerHTML={{ __html: project.html || '' }} />
